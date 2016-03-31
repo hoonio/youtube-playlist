fa29f24e0865eead6b26e8e28730df6ffc70910a
Hoonio <hoonio@hoonio.com>
2016-03-31 03:41:40 +0100

    support youtube video load over https

--- app/detailCtrl.js
+++ app/detailCtrl.js
@@ -3,5 +3,5 @@ app.controller('DetailCtrl', ['$scope', 'videos', '$routeParams', '$sce',
     console.log('processing detail view' + $routeParams.videoid)
     $scope.name = "My YouTube Playlist";
     $scope.detail = videos.detail($routeParams.videoid)
-    $scope.videoUrl = $sce.trustAsResourceUrl('http://www.youtube.com/embed/'+$scope.detail.snippet.resourceId.videoId)
+    $scope.videoUrl = $sce.trustAsResourceUrl('//www.youtube.com/embed/'+$scope.detail.snippet.resourceId.videoId)
 }]);
